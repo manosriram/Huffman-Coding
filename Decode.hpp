2cf2bdb8041c40352d362d8d9cd0541de43faf92
Mano Sriram <mano.sriram0@gmail.com>
2019-11-23 07:35:51 +0530

    Add Encode and Decode. Last Character bug not fixed.

--- Decode.hpp
+++ Decode.hpp
@@ -2,20 +2,42 @@
 #define DEC
 #include "HuffStruct.hpp"
 bool taken = false;
+#include <bitset>
 
 string Decode(MinHeap *root) {
 
-    file.open("out.txt", ios::in | ios::out); 
-    
+    file.open("out.txt", ios::in | ios::out);
+    string dec1;
     if (file.is_open()) {
         string tp;
         while (getline(file, tp)) {
-            for (int t=0;t<tp.length();t++)
-                cout << (unsigned char)tp[t] << " ";
+            for (int t=0;t<tp.length();t++) {
+                int x = (int)tp[t];
+                if (x < 0)
+                    x += 256;
+
+                dec1 += bitset<8>(x).to_string();
+            }
         }
-    } 
-    
-    
-    return "1213";
+        file.close();
+    }
+
+    MinHeap *Node = root;
+    int n = dec1.end() - dec1.begin();
+    string out = "";
+    for (int t=0;t<=n;t++) {
+        if (dec1[t] == '0')
+            root = root->left;
+        else
+            root = root->right;
+
+        if (root->isLeaf) {
+            out += root->character;
+            root = Node;
+        }
+    }
+    out += '\0';
+    cout << out;
+    return out;
 }
 #endif
--- Encode.hpp
+++ Encode.hpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include "HuffStruct.hpp"
 #include <map>
+#include <bitset>
 #include <vector>
 #include "BuildTree.hpp"
 fstream file;
@@ -27,62 +28,27 @@ void search(char c, MinHeap *root) {
     search(c, root->right);
     path.pop_back();
 }
+
 unsigned char ch;
 int bitC = 0;
-
-int reverseByte(int num) {
-    unsigned int rev = 0;
-    while (num) {
-        rev <<= 1;
-        if (num & 1)
-            rev ^= 1;
-
-        num >>= 1;
-    }
-    return rev;
-}
-
-void writeByte(int byte) {
-    //byte = reverseByte(byte);
-    int n = sizeof(int);
-    while (byte) {
-        file << (byte & 1);
-        byte >>= 1;
-        n--;
-    }
-    while (n > 0) {
-        file << 0;
-        n--;
-    }
-    return;
-}
-
-
 void writeBit(int bit) {
     ch = (ch << 1) | bit;
-    //cout << "Ch: " << (int)ch << endl;
     if (++bitC == 8) {
         int sz = 8; 
-        while (ch) {
-            file << (ch & 1);
-            ch >>= 1;
-            sz--;
-        }
-        while (sz--) {
-            file << 0;
-        }
-        bitC = 0;
+        file << ch;
         ch = 0;
+        bitC = 0;
     }
 }
 MinHeap *Encode(map<char, pair<int, string> > mp,
         priority_queue<pair< double, MinHeap *>, vector<pair<double, MinHeap *> >, greater<pair< double, MinHeap *>> > q, MinHeap *root) {
-    file.open("out.txt", ios::out);
+    file.open("out.txt", ios::out | ios::binary);
     for (auto t = mp.begin(); t!=mp.end();++t) {
         search(t->first, root);
         t->second.second = v[0];
         v.clear();
     }
+
     for (int t=0;t<txt.length();t++) {
         int n = mp[txt[t]].second.end() - mp[txt[t]].second.begin();
         for (int t1=0;t1<mp[txt[t]].second.length();t1++) {
@@ -92,10 +58,10 @@ MinHeap *Encode(map<char, pair<int, string> > mp,
                 writeBit(1);
             else
                 writeBit(0);
-            // cout << temp;
         }
     }
     file.close();
+    cout << endl;
     return root;
 }
 #endif
